0bc0f07bc4507785ac572bd9d74d3ac2068ad6ca
Dagim Abraham <dagimabraham79@gmail.com>
2025-03-18 23:10:42 +0300

    initial commit -Apple Home Page With React

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import  { useState } from 'react'
+import "./commonResource/css/styles.css"
+import "./commonResource/css/bootstrap.css"
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Alert from './components/Alert.jsx'
+import FirstSection from './components/FirstSection.jsx'
+import SecondSection from "./components/SecondSection.jsx"
+import ThirdSection from './components/ThirdSection.jsx'
+import FourthSection from './components/FourthSection.jsx'
+import FifthSection from './components/FifthSection.jsx'
+import SixthSection from './components/SixthSection.jsx'
+import YouTubeVideos from './components/YouTube/YouTubeVideos.jsx'
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Alert />
+      <FirstSection />
+      <SecondSection/>
+      <ThirdSection/>
+      <FourthSection/>
+      <FifthSection/>
+      <SixthSection/>
+      <YouTubeVideos />
+      <Footer />
+    </>
+  );
+}
+
+export default App
---
+++ src/components/YouTube/YouTubeVideos.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+
+function YouTubeVideos() {
+  const [youTubeVideos, setYouTubeVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCxA7AzkI2Sndf8S1G5rSkwQ&maxResults=5&order=date&key=AIzaSyDvFfXkI13YSywDLiCAe4W6GoPOhipr6Nc"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setYouTubeVideos(data.items);
+      });
+  }, []);
+
+  return (
+    <div className="videosWrapper">
+      <div className="allVideosWrapper">
+        <div className="container">
+          <div className="row justify-content-center ">
+            <div className="col-12">
+              <div className="tittle-wrapper">
+                <br />
+                <h1>Latest video</h1>
+                <br />
+              </div>
+            </div>
+
+            {youTubeVideos?.map((singleVideo, i) => {
+              let vidId = singleVideo.id.videoId;
+              let vidLink = `https://youtube.com/watch?v=${vidId}`;
+              let videoWrapper = (
+                <div key={i} className="col-sm-12 col-md-6 col-lg-4">
+                  <div className="singleVideoWrapper">
+                    <div className="videoThumbnail">
+                      <a href={vidLink} target="_blank" rel="noreferrer">
+                        <img
+                          src={singleVideo.snippet.thumbnails.high.url}
+                          alt="thumbnails"
+                        />
+                      </a>
+                    </div>
+
+                    <div className="videoInfoWrapper">
+                      <div className="videoTitle">
+                        <a href={vidLink} target="_blank" rel="noreferrer">
+                          {singleVideo.snippet.title}
+                        </a>
+                      </div>
+                      <div className="videoDescription">
+                        {singleVideo.snippet.description}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+              return videoWrapper;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default YouTubeVideos;
